b722c70d878a0803d0511c6b1b29c65682813335
panayotkulchev <panayotkulchev@gmail.com>
2015-12-13 22:18:59 +0200

    menu closes on click

--- src/main/java/com/clouway/ta/core/SidFetcher.java
+++ src/main/java/com/clouway/ta/core/SidFetcher.java
@@ -30,7 +30,6 @@ public class SidFetcher {
         }
 
         for (Cookie each : cookies) {
-          System.out.println(each.getName());
             if (each.getName().equalsIgnoreCase("sid")) {
                 return each.getValue();
             }
