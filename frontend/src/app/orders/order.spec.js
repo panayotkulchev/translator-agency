89f5f90a676ca92eb491c3a83650e81c04729a4e
panayotkulchev <panayotkulchev@gmail.com>
2016-04-24 17:39:01 +0300

    frontend/orders: tests for  'orders list' page

--- frontend/src/app/orders/order.spec.js
+++ frontend/src/app/orders/order.spec.js
@@ -24,7 +24,8 @@ describe('Orders', function () {
       });
     }));
 
-    it('doesn\'t have any assertions', function(){});
+    it('doesn\'t have any assertions', function () {
+    });
 
     it('search client', function () {
       state.go = jasmine.createSpy('state.go');
@@ -32,7 +33,7 @@ describe('Orders', function () {
 
       scope.searchClient(clientId);
 
-      expect(state.go).toHaveBeenCalledWith('clients',{clientId: clientId});
+      expect(state.go).toHaveBeenCalledWith('clients', {clientId: clientId});
     });
 
     it('edit order', function () {
@@ -41,7 +42,7 @@ describe('Orders', function () {
 
       scope.editOrder(orderId);
 
-      expect(state.go).toHaveBeenCalledWith('orderEditor',{orderId: orderId});
+      expect(state.go).toHaveBeenCalledWith('orderEditor', {orderId: orderId});
     });
 
     it('go to order editor', function () {
@@ -51,6 +52,87 @@ describe('Orders', function () {
 
       expect(state.go).toHaveBeenCalledWith('orderEditor');
     });
+
+    it('show more orders', function () {
+      var filter = "OPENED";
+      var offset = 10;
+      var limit = 20;
+
+      scope.orderList = [{id: 1111}, {id: 2222}];
+      var response = [{id: 3333}, {id: 4444}, {id: 5555}];
+      var expected = [{id: 1111}, {id: 2222}, {id: 3333}, {id: 4444}];
+
+      scope.pager = {
+        accept: jasmine.createSpy("pagerAccept"),
+        pageSize: 2
+      };
+
+      ordersGateway.searchOrders = jasmine.createSpy("searchOrders").and.returnValue(deferred.promise);
+
+      scope.showMore(filter, offset, limit);
+
+      deferred.resolve(response);
+      scope.$digest();
+
+      expect(ordersGateway.searchOrders).toHaveBeenCalledWith(filter, offset, limit);
+      expect(scope.pager.accept).toHaveBeenCalledWith(response);
+      expect(scope.orderList).toEqual(expected);
+    });
+
+    it('page do not change when got error when load more orders', function () {
+      var filter = "OPENED";
+      var offset = 10;
+      var limit = 20;
+
+      scope.orderList = [{id: 1111}, {id: 2222}];
+      var expected = [{id: 1111}, {id: 2222}];
+
+      scope.pager = {
+        rollback: jasmine.createSpy("pagerRollback"),
+        pageSize: 2
+      };
+
+      ordersGateway.searchOrders = jasmine.createSpy("searchOrders").and.returnValue(deferred.promise);
+
+      scope.showMore(filter, offset, limit);
+
+      deferred.reject();
+      scope.$digest();
+
+      expect(ordersGateway.searchOrders).toHaveBeenCalledWith(filter, offset, limit);
+      expect(scope.orderList).toEqual(expected);
+    });
+
+    it('filter value changed', function () {
+      var filter = "OPENED";
+      var offset = 10;
+      var limit = 20;
+
+      scope.orderList = [{id: 1111}, {id: 2222}];
+      var response = [{id: 3333}, {id: 4444}, {id: 5555}];
+      var expected = [{id: 3333}, {id: 4444}];
+
+      scope.pager = {
+        offset: offset,
+        count: limit + 1, // count is pageSize + 1 to check if next page is available
+        accept: jasmine.createSpy("pagerAccept"),
+        reset: jasmine.createSpy("pagerReset"),
+        pageSize: 2
+      };
+
+      ordersGateway.searchOrders = jasmine.createSpy("searchOrders").and.returnValue(deferred.promise);
+
+      scope.onFilterChange(filter);
+
+      deferred.resolve(response);
+      scope.$digest();
+
+      expect(scope.pager.reset).toHaveBeenCalled();
+      expect(ordersGateway.searchOrders).toHaveBeenCalledWith(filter, scope.pager.offset, scope.pager.count);
+      expect(scope.pager.accept).toHaveBeenCalledWith(response);
+      expect(scope.orderList).toEqual(expected);
+    });
+
   });
 
 });
